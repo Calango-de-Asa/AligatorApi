aec805fc0036754c7b40c26a3e85ece05571f98e
Skalnark <skalnark@live.com>
2020-09-27 18:22:31 -0300

    migration added

---
+++ AligatorApi/Migrations/20200925023051_populatedb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AligatorApi.Migrations
+{
+    public partial class populatedb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+
+        }
+    }
+}
