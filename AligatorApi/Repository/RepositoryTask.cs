16ffe6ae8e573efb7694b0bd33b5332e5aa4e9d0
Skalnark <skalnark@live.com>
2020-09-27 23:16:36 -0300

    Task type changed to Aligator.Models.Task

--- AligatorApi/Repository/RepositoryTask.cs
+++ AligatorApi/Repository/RepositoryTask.cs
@@ -1,15 +1,11 @@
 ﻿using AligatorApi.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AligatorApi.Models;
 
 namespace AligatorApi.Repository
 {
     public class RepositoryTask : Repository<Task>, IRepositoryTask
     {
-        public RepositoryTask(DatabaseContext context) 
-            : base(context)
+        public RepositoryTask(DatabaseContext context) : base(context)
         { }
     }
 }
